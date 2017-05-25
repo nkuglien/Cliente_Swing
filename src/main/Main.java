ce7a45e00e56d558f1c37368108fb87bb307c935
nico_ <nico_@DESKTOP-UI6V6UM>
2017-05-25 16:23:27 -0300

    First Commit

---
+++ src/main/Main.java
@@ -0,0 +1,38 @@
+package main;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import DTO.ClienteDTO;
+import RemoteObject.ClienteRemote;
+
+public class Main {
+public static ClienteRemote remoto;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+try{
+		if(getStub()){
+			System.out.println("Se conecto");
+			List<ClienteDTO> clientes = remoto.GetClientes();
+			System.out.println("Levante los clientes");
+		}
+		else
+			System.out.println("No se conecto");
+}
+catch (Exception e) {
+	System.out.println(e.getMessage());
+}
+		
+	}
+	 public static boolean getStub() {
+	    	
+	    	try {
+				remoto = (ClienteRemote)Naming.lookup ("//localhost/GestionClientes");
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return false;
+	    }
+}
